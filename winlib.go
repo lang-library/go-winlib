d3c2fea5c8f6875f3b83255edd26aad1df658a28
JavaCommons Technologies <javacommons@gmail.com>
2024-01-30 00:09:27 +0900

    Release v0.1.8

--- winlib.go
+++ winlib.go
@@ -7,17 +7,17 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func StringToUTF16Addr(s string) uintptr {
-	arr := StringToUTF16Ptr(s)
+func StringToWideAddr(s string) uintptr {
+	arr := StringToWidePtr(s)
 	return uintptr(unsafe.Pointer(arr))
 }
 
-func UTF16AddrToString(s uintptr) string {
+func WideAddrToString(s uintptr) string {
 	p := (*uint16)(unsafe.Pointer(s))
-	return UTF16PtrToString(p)
+	return WidePtrToString(p)
 }
 
-func StringToUTF16Ptr(s string) *uint16 {
+func StringToWidePtr(s string) *uint16 {
 	p, err := windows.UTF16PtrFromString(s)
 	if err != nil {
 		return nil
@@ -25,7 +25,7 @@ func StringToUTF16Ptr(s string) *uint16 {
 	return p
 }
 
-func UTF16PtrToString(p *uint16) string {
+func WidePtrToString(p *uint16) string {
 	return windows.UTF16PtrToString(p)
 }
 
